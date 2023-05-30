fc55b6805d90e94d1edb8fa815ffdc186d90e355
vijay gottipati <66865540+f20190266@users.noreply.github.com>
2023-05-30 22:10:49 +0400

    Capture image.py

--- Capture image.py
+++ Capture image.py
@@ -1,34 +1,79 @@
 import numpy as np
 import cv2
+import os
+import time as t
 
+if not os.path.exists('images'):
+os.makedirs('images')
+
+face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 capture = cv2.VideoCapture(0)
-face = cv2.CascadeClassifier(cv2.data.haarcascades +
-'haarcascade_frontalface_default.xml')
-eye = cv2.CascadeClassifier(cv2.data.haarcascades +
-'haarcascade_eye.xml')
+capture.set(3,640)
+capture.set(4,480)
+count = 0
+face_detector =
+cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-while True:
+faceid = input('\n Enter your Roll no : ')
+print("\n Initializing face capture. Look the camera and wait ....")
 
-ret,box = capture.read()
-
-grayscale = cv2.cvtColor(box, cv2.COLOR_BGR2GRAY)
-
-11
-faces = face.detectMultiScale(grayscale,1.3,5) #2nd factor to
-reduce the size of our face to match the classifier , the lesser it is the
-better accuracy but slower algo perform
+while(True):
+ret, img = capture.read()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
-cv2.rectangle(box,(x,y),(x+w,y+h),(255,0,0),5) #3rd param colour
-BGR and last param line thickness
-gray = grayscale[y:y+h , x:x+w]
-color = box[y:y+h , x:x+w]
-eyes = eye.detectMultiScale(gray,1.3,5)
-for (ex,ey,ew,eh) in eyes:
-cv2.rectangle(color,(ex,ey),(ex+ew,ey+eh),(0,255,0),5)
+cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+count += 1
 
-cv2.imshow('frame', box)
+cv2.imwrite("./images/Users." + str(faceid) + '.' + str(count) + ".jpg",
+gray[y:y+h,x:x+w])
+cv2.imshow('image', img)
 
-if cv2.waitKey(1) == ord('x'): # press 'x' to exit the capturing screen
+k = cv2.waitKey(100) & 0xff
+if k < 90:
 break
+elif count >= 90:
+break
+t.sleep(3)
+
+while(True):
+ret, img = capture.read()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+count += 1
+
+cv2.imwrite("./images/Users." + str(faceid) + '.' + str(count) + ".jpg",
+gray[y:y+h,x:x+w])
+cv2.imshow('image', img)
+
+k = cv2.waitKey(100) & 0xff
+if k < 60:
+break
+elif count >= 60:
+
+break
+t.sleep(3)
+
+while(True):
+ret, img = capture.read()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+count += 1
+
+cv2.imwrite("./images/Users." + str(faceid) + '.' + str(count) + ".jpg",
+gray[y:y+h,x:x+w])
+cv2.imshow('image', img)
+
+k = cv2.waitKey(100) & 0xff
+if k < 90:
+break
+elif count >= 90:
+break
+
+print("\n Exiting Program......")
 capture.release()
 cv2.destroyAllWindows()
